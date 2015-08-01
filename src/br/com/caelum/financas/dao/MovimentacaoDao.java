de8d2c6151b3edf3286338308539e0ad18f6db85
Bruno Souza <brunoprogramadorjava>
2015-08-01 16:12:58 -0300

    Buscando movimentacoes segundo alguns criterios

--- src/br/com/caelum/financas/dao/MovimentacaoDao.java
+++ src/br/com/caelum/financas/dao/MovimentacaoDao.java
@@ -11,6 +11,7 @@ import javax.persistence.Query;
 import br.com.caelum.financas.exception.ValorInvalidoException;
 import br.com.caelum.financas.modelo.Conta;
 import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
 
 @Stateless
 public class MovimentacaoDao {
@@ -23,6 +24,16 @@ public class MovimentacaoDao {
 				+ "where  m.conta = :conta order by m.valor desc";
 		Query query = this.manager.createQuery(jpql);
 		query.setParameter("conta", conta);
+		return query.getResultList();
+	}
+	
+	public List<Movimentacao> listaPorValorETipo(BigDecimal valor, TipoMovimentacao tipo){
+		String jpql = "select m from Movimentacao m " +
+					"where m.valor = :valor and m.tipoMovimentacao = :tipo";
+		
+		Query query = this.manager.createQuery(jpql);
+		query.setParameter("valor", valor);
+		query.setParameter("tipo", tipo);			
 		return query.getResultList();
 	}
 
