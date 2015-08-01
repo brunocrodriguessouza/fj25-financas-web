0460c9003afb7042bd359e3cc6bb510adf9111ab
Bruno Souza <brunoprogramadorjava>
2015-08-01 15:02:55 -0300

    Manipulando a transacao com EJB3

--- src/br/com/caelum/financas/dao/MovimentacaoDao.java
+++ src/br/com/caelum/financas/dao/MovimentacaoDao.java
@@ -1,11 +1,13 @@
 package br.com.caelum.financas.dao;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import br.com.caelum.financas.exception.ValorInvalidoException;
 import br.com.caelum.financas.modelo.Movimentacao;
 
 @Stateless
@@ -16,6 +18,11 @@ public class MovimentacaoDao {
 
 	public void adiciona(Movimentacao movimentacao) {
 		this.manager.persist(movimentacao);
+
+		if (movimentacao.getValor().compareTo(BigDecimal.ZERO) < 0) {
+//			throw new RuntimeException("Movimentacao negativa");
+			throw new ValorInvalidoException("Movimentacao negativa");
+		}
 	}
 
 	public Movimentacao busca(Integer id) {
@@ -23,11 +30,13 @@ public class MovimentacaoDao {
 	}
 
 	public List<Movimentacao> lista() {
-		return this.manager.createQuery("select m from Movimentacao m", Movimentacao.class).getResultList();
+		return this.manager.createQuery("select m from Movimentacao m",
+				Movimentacao.class).getResultList();
 	}
 
 	public void remove(Movimentacao movimentacao) {
-		Movimentacao movimentacaoParaRemover = this.manager.find(Movimentacao.class, movimentacao.getId());
+		Movimentacao movimentacaoParaRemover = this.manager.find(
+				Movimentacao.class, movimentacao.getId());
 		this.manager.remove(movimentacaoParaRemover);
 	}
 
